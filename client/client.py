ef1b21b5e431ba7f478b104975d0d7c4314f090a
Tobias Park <tobiasjpark73@gmail.com>
2022-03-21 21:14:10 -0400

    Client code debugging

--- client/client.py
+++ client/client.py
@@ -29,80 +29,85 @@ class Client:
         while True:
             print("Checking for commands from server...")
             completed_round = False
+            version = -1
             try:
                 self.service_client.download_file('client-assignments', self.CLIENT_ID, '/tmp/' + self.CLIENT_ID + 'tmp.txt')
                 version = int(open('/tmp/' + self.CLIENT_ID + 'tmp.txt', 'r').read()) + 1
-                
-                print("Participating in new round...")
-                print("Creating new Version " + str(version))
-
-                version_file = None 
-                try:
-                    version_file = open(self.CLIENT_ID + 'version.txt', 'r')
-                except:
-                    # file does not exist; just say 
-                    version_file = open(self.CLIENT_ID +'version.txt', 'w')
-                    version_file.write("0")
-                    version_file.close()
-                    version_file = open(self.CLIENT_ID +'version.txt', 'r')
-                current_version = int(version_file.read())
-
-                if current_version >= version:
-                    print("Already processed this version")
-                    sleepTilNextRound(completed_round)
-                    continue # no update, we already processed this
-
-                timestamps = {}
-                timestamps["T1"] = time()
-
-                print("Downloading current model version from server")
-                self.service_client.download_file('global-server-model', str(version-1), self.CLIENT_ID + 'tmp_model.nn')
-                timestamps["T2"] = time()
-                timestamps["T3 Download Time"] = timestamps["T2"] - timestamps["T1"] 
-                net_file = open(self.CLIENT_ID + 'tmp_model.nn', 'rb')
-                net_dict = pickle.load(net_file)
-                my_net = Net() 
-                my_net.load_state_dict(net_dict)
-
-                # Use GPU if available
-                device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-                my_net.to(device)
-                cudnn.benchmark = True
-                
-                print("Training locally...")
-                # Perform update step locally
-                my_net = train(my_net, self.client_data, epoch) 
-
-                timestamps["T4"] = time()
-                timestamps["T5 Compute Time"] = timestamps["T4"] - timestamps["T2"] 
-
-                print("Training finished, uploading new model to server")
-
-                # Upload to server
-                my_net_dict = createPickle(my_net, self.client_data)
-                pickle.dump(my_net_dict, open(self.CLIENT_ID + 'tmp_net.nn', 'wb'))
-                upload_filename = str(str(version) + "-" + str(self.CLIENT_ID))
-                self.service_client.upload_file(self.CLIENT_ID + 'tmp_net.nn', 'client-weights', upload_filename)
-
-                version_file = open(self.CLIENT_ID + 'version.txt', 'w')
-                version_file.write(str(version))
-                version_file.close()
-                completed_round = True
-                print("Done")
-
-                # get S3 upload timestamp
-                response = self.service_client.head_object(Bucket="client-weights", Key=upload_filename)
-                t = response["LastModified"]
-                t = t.replace(tzinfo=timezone.utc).astimezone(tz=None) # convert from UTC to local time so the timestamps match up for the Upload Time calculation
-                timestamps["T6"] = mktime(t.timetuple()) + t.microsecond / 1E6
-                timestamps["Upload Time"] = timestamps["T6"] - timestamps["T4"]
-
-                dyn_table = boto3.client('dynamodb')
-                for timestamp in timestamps:
-                    dyn_table.update_item(TableName='timestamps', Key={'Version': {'N': str(version)}}, AttributeUpdates={self.CLIENT_ID + '-' + timestamp: {'Value': {'N': str(timestamps[timestamp])}}})
-
             except:
-                pass
+                continue
+
+            if version == -1:
+                continue
+
+            print("Participating in new round...")
+            print("Creating new Version " + str(version))
+
+            version_file = None 
+            try:
+                version_file = open(self.CLIENT_ID + 'version.txt', 'r')
+            except:
+                # file does not exist; just say 
+                version_file = open(self.CLIENT_ID +'version.txt', 'w')
+                version_file.write("0")
+                version_file.close()
+                version_file = open(self.CLIENT_ID +'version.txt', 'r')
+            current_version = int(version_file.read())
+
+            if current_version >= version:
+                print("Already processed this version")
+                sleepTilNextRound(completed_round)
+                continue # no update, we already processed this
+
+            timestamps = {}
+            timestamps["T1"] = time()
+
+            print("Downloading current model version from server")
+            self.service_client.download_file('global-server-model', str(version-1), self.CLIENT_ID + 'tmp_model.nn')
+            timestamps["T2"] = time()
+            timestamps["T3 Download Time"] = timestamps["T2"] - timestamps["T1"] 
+            net_file = open(self.CLIENT_ID + 'tmp_model.nn', 'rb')
+            net_dict = pickle.load(net_file)
+            my_net = Net() 
+            my_net.load_state_dict(net_dict)
+
+            # Use GPU if available
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            my_net.to(device)
+            cudnn.benchmark = True
+            
+            print("Training locally...")
+            # Perform update step locally
+            my_net = train(my_net, self.client_data, epoch) 
+
+            timestamps["T4"] = time()
+            timestamps["T5 Compute Time"] = timestamps["T4"] - timestamps["T2"] 
+
+            print("Training finished, uploading new model to server")
+
+            # Upload to server
+            my_net_dict = createPickle(my_net, self.client_data)
+            pickle.dump(my_net_dict, open(self.CLIENT_ID + 'tmp_net.nn', 'wb'))
+            upload_filename = str(str(version) + "-" + str(self.CLIENT_ID))
+            self.service_client.upload_file(self.CLIENT_ID + 'tmp_net.nn', 'client-weights', upload_filename)
+
+            version_file = open(self.CLIENT_ID + 'version.txt', 'w')
+            version_file.write(str(version))
+            version_file.close()
+            completed_round = True
+            print("Done")
+
+            # get S3 upload timestamp
+            response = self.service_client.head_object(Bucket="client-weights", Key=upload_filename)
+            t = response["LastModified"]
+            t = t.replace(tzinfo=timezone.utc).astimezone(tz=None) # convert from UTC to local time so the timestamps match up for the Upload Time calculation
+            timestamps["T6"] = mktime(t.timetuple()) + t.microsecond / 1E6
+            timestamps["Upload Time"] = timestamps["T6"] - timestamps["T4"]
+
+            dyn_table = boto3.client('dynamodb')
+            for timestamp in timestamps:
+                dyn_table.update_item(TableName='timestamps', Key={'Version': {'N': str(version)}}, AttributeUpdates={self.CLIENT_ID + '-' + timestamp: {'Value': {'N': str(timestamps[timestamp])}}})
+
+            
 
             sleepTilNextRound(completed_round)
 
